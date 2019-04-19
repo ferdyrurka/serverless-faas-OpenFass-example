551fd7a3bf41725be22597043f6bd785207611af
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-04-19 21:51:46 +0200

    Update Handler

--- src/hello-csharp/FunctionHandler.cs
+++ src/hello-csharp/FunctionHandler.cs
@@ -6,7 +6,7 @@ namespace Function
     public class FunctionHandler
     {
         public string Handle(string input) {
-            return $"Hello World in Open faas and C#";
+            return "Hello World in Open faas and C#";
         }
     }
 }
