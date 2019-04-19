46fc5e2b09ba702844be50ed2295060e2f5dc502
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-04-19 21:17:39 +0200

    HelloCsharp function

---
+++ src/hello-csharp/FunctionHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public string Handle(string input) {
+            return $"Hello World in Open faas and C#";
+        }
+    }
+}
