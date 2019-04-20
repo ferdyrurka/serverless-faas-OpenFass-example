e80983731f596c20d3bde269f9df1ddedacc62a1
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-04-20 23:27:06 +0200

    NotFoundException

---
+++ src/search-user/src/Exception/NotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+/**
+ * Class UserNotFoundException
+ * @package App\Exception
+ */
+class NotFoundException extends SearchUserException
+{
+}
