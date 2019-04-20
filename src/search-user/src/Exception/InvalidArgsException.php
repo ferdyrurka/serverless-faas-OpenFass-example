b93950644002b428874845b4f19b282dfe78cb51
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-04-20 23:46:01 +0200

    InvalidArgsException

---
+++ src/search-user/src/Exception/InvalidArgsException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+/**
+ * Class InvalidArgsException
+ * @package App\Exception
+ */
+class InvalidArgsException extends SearchUserException
+{
+}
