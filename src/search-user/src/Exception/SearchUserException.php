1819825792eb2d99b914ee6a922eabcd28bbea48
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-04-20 23:23:01 +0200

    SearchUserException

---
+++ src/search-user/src/Exception/SearchUserException.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+use \Exception;
+
+/**
+ * Class SearchUserException
+ * @package App\Exception
+ */
+class SearchUserException extends Exception
+{
+}
