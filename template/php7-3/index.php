1f28fcf5848174fa447460b21e5c5cebd487c4ae
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-04-29 22:19:35 +0200

    Fix empty args to array convert

--- template/php7-3/index.php
+++ template/php7-3/index.php
@@ -8,7 +8,11 @@ if (file_exists('function/vendor/autoload.php')) {
 }
 
 $stdin = file_get_contents("php://stdin");
-$arrayStdin = \json_decode($stdin, true);
+$arrayStdin = [];
+
+if (!empty($stdin)) {
+    $arrayStdin = \json_decode($stdin, true);
+}
 
 $response = (new App\Handler())->handle($arrayStdin);
 echo $response;
