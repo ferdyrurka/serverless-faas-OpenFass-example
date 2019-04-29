90a6c82b5baf8db0736043ae97a8705397534b57
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-04-29 20:39:42 +0200

    Rename directory and index.php

---
+++ template/php7-3/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Requires Function composer's autoload
+if (file_exists('function/vendor/autoload.php')) {
+    require('function/vendor/autoload.php');
+} else {
+    throw new Exception('File not exist function/vendor/autoload.php!');
+}
+
+$stdin = file_get_contents("php://stdin");
+$arrayStdin = \json_decode($stdin, true);
+
+$response = (new App\Handler())->handle($arrayStdin);
+echo $response;
