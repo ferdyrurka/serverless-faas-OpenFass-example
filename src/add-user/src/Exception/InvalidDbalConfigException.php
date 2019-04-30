f8f902ac6ce643e61b5f7587956e025ffe294b9c
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-04-30 22:41:18 +0200

    InvalidDbalConfigException

---
+++ src/add-user/src/Exception/InvalidDbalConfigException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+/**
+ * Class InvalidDbalConfigException
+ * @package App\Exception
+ */
+class InvalidDbalConfigException extends HttpException
+{
+    /**
+     * InvalidDbalConfigException constructor.
+     * @param string $message
+     */
+    public function __construct($message = '')
+    {
+        parent::__construct($message, 500);
+    }
+}
