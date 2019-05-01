6f68547d3772adb2a4bf4e32d81041a7b1cc0ea6
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-01 21:47:40 +0200

    UserRepository

--- src/users/src/Repository/UserRepository.php
+++ src/users/src/Repository/UserRepository.php
@@ -3,8 +3,44 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Common\Database\ConnectionDatabase;
+use App\Entity\User;
+use Doctrine\DBAL\Query\QueryBuilder;
 
+/**
+ * Class UserRepository
+ * @package App\Repository
+ */
 class UserRepository
 {
+    /**
+     * @var QueryBuilder
+     */
+    private $queryBuilder;
 
+    /**
+     * UserRepository constructor.
+     */
+    public function __construct()
+    {
+        $connectionDatabase = new ConnectionDatabase();
+        $this->queryBuilder = $connectionDatabase->getConnection()->createQueryBuilder();
+    }
+
+    /**
+     * @param User $user
+     */
+    public function save(User $user): void
+    {
+        $this->queryBuilder
+            ->insert('users')
+            ->values([
+                'username' => '?',
+                'created_at' => '?'
+            ])
+            ->setParameter(0, $user->getUsername())
+            ->setParameter(1, $user->getCreatedAt())
+            ->execute()
+        ;
+    }
 }
