7b2bdab3d236d1f72c83f8fc901f8f2525aea4ce
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-01 21:26:26 +0200

    CreateUserDataValidator

--- src/users/src/Common/Validator/CreateUserDataValidator.php
+++ src/users/src/Common/Validator/CreateUserDataValidator.php
@@ -3,10 +3,54 @@ declare(strict_types=1);
 
 namespace App\Common\Validator;
 
-class UserDataValidator
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Validation;
+
+class CreateUserDataValidator implements DataValidatorInterface
 {
+    /**
+     * @var array
+     */
+    private $errors = [];
+
     public function validate(array $data): bool
     {
+        if (!isset($data['username'])) {
+            $this->errors[] = 'Username is required parameters!';
 
+            return false;
+        }
+
+        $validator = Validation::createValidator();
+        $violations = $validator->validate($data['username'], [
+           new Length(
+               [
+                   'min' => 6,
+                   'minMessage' => 'Minimum username length is 6 chars',
+                   'max' => 64,
+                   'maxMessage' => 'Maximum username length is 6 chars',
+               ]
+           ),
+           new NotBlank()
+        ]);
+
+        if (\count($violations) !== 0) {
+            foreach ($violations as $violation) {
+                $this->errors[] = $violation->getMessage();
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
     }
 }
---
+++ src/users/tests/Common/Validator/CreateUserDataValidatorTest.php
@@ -0,0 +1,123 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Common\Validator;
+
+use App\Common\Validator\CreateUserDataValidator;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use \Mockery;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * Class CreateUserDataValidatorTest
+ * @package App\Tests\Common\Validator
+ */
+class CreateUserDataValidatorTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @var CreateUserDataValidator
+     */
+    private $createUserDataValidator;
+
+    /**
+     *
+     */
+    protected function setUp(): void
+    {
+        $this->createUserDataValidator = new CreateUserDataValidator();
+    }
+
+    /**
+     * @test
+     */
+    public function notRequiredParameters(): void
+    {
+        $this->assertFalse(
+            $this->createUserDataValidator->validate([])
+        );
+        $this->assertNotEmpty(
+            $this->createUserDataValidator->getErrors()
+        );
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disable
+     */
+    public function validatorFailed(): void
+    {
+        $violation = Mockery::mock(ConstraintViolationInterface::class);
+        $violation->shouldReceive('getMessage')->once()->andReturn('Message');
+
+        $this->setUpValidator([$violation]);
+
+        $this->assertFalse(
+            $this->createUserDataValidator->validate(
+                [
+                    'username' => 'UsernameValue'
+                ]
+            )
+        );
+        $this->assertNotEmpty(
+            $this->createUserDataValidator->getErrors()
+        );
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disable
+     */
+    public function validateOk(): void
+    {
+        $this->setUpValidator();
+
+        $this->assertTrue(
+            $this->createUserDataValidator->validate(
+                [
+                    'username' => 'UsernameValue'
+                ]
+            )
+        );
+        $this->assertEmpty(
+            $this->createUserDataValidator->getErrors()
+        );
+    }
+
+    /**
+     * @param array $returnValidate
+     */
+    private function setUpValidator(array $returnValidate = []): void
+    {
+        $validator = Mockery::mock(ValidatorInterface::class);
+        $validator->shouldReceive('validate')->once()
+            ->withArgs(
+                function (string $username, array $constraints): bool {
+                    if ($username !== 'UsernameValue' ||
+                        !isset($constraints[0], $constraints[1]) ||
+                        !$constraints[0] instanceof Length ||
+                        !$constraints[1] instanceof NotBlank
+                    ) {
+                        return false;
+                    }
+
+                    return true;
+                }
+            )
+            ->andReturn($returnValidate)
+        ;
+
+        $validation = Mockery::mock('alias:' . Validation::class);
+        $validation->shouldReceive('createValidator')->once()
+            ->andReturn($validator)
+        ;
+    }
+}
