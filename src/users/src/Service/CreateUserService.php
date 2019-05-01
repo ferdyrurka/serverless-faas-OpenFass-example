9dfefe54b8d323af3c5e817fd4ab3306d6d0ad24
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-01 22:59:45 +0200

    CreateUserService

--- src/users/src/Service/CreateUserService.php
+++ src/users/src/Service/CreateUserService.php
@@ -1,14 +1,29 @@
 <?php
 declare(strict_types=1);
 
-
 namespace App\Service;
 
+use App\Common\Model\ResponseModel;
+use App\Common\Validator\CreateUserDataValidator;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use \DateTime;
 
-class UserService
+class CreateUserService implements UserServiceInterface
 {
-    public function createUser(array $data): string
+    public function handle(array $data): ResponseModel
     {
+        $createUserDataValidator = new CreateUserDataValidator();
+        if (!$createUserDataValidator->validate($data)) {
+            return new ResponseModel(400, $createUserDataValidator->getErrors());
+        }
 
+        $date = new DateTime('now');
+        $user = new User($data['username'], $date->getTimestamp());
+
+        $userRepository = new UserRepository();
+        $userRepository->save($user);
+
+        return new ResponseModel(200, ['success' => true]);
     }
 }
---
+++ src/users/tests/Service/CreateUserServiceTest.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Service;
+
+use App\Common\Validator\CreateUserDataValidator;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Service\CreateUserService;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use \Mockery;
+
+/**
+ * Class CreateUserServiceTest
+ * @package App\Tests\Service
+ */
+class CreateUserServiceTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @var CreateUserService
+     */
+    private $createUserService;
+
+    /**
+     * @var CreateUserDataValidator
+     */
+    private $createUserDataValidator;
+
+    /**
+     *
+     */
+    protected function setUp(): void
+    {
+        $this->createUserService = new CreateUserService();
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function validateFailed(): void
+    {
+        $this->setUpCreateUserDataValidator(false);
+        $this->createUserDataValidator->shouldReceive('getErrors')->once()->andReturn([]);
+
+        $responseModel = $this->createUserService->handle(['username' => 'UsernameValue']);
+        $this->assertEquals(400, $responseModel->getStatusCode());
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function handleOk(): void
+    {
+        $this->setUpCreateUserDataValidator(true);
+
+        $userRepository = Mockery::mock('overload:' . UserRepository::class);
+        $userRepository->shouldReceive('save')->withArgs([User::class])->once();
+
+        $responseModel = $this->createUserService->handle(['username' => 'UsernameValue']);
+        $this->assertEquals(200, $responseModel->getStatusCode());
+        $this->assertTrue($responseModel->getBody()['success']);
+    }
+
+    /**
+     * @param bool $returnValidate
+     */
+    private function setUpCreateUserDataValidator(bool $returnValidate): void
+    {
+        $this->createUserDataValidator = Mockery::mock('overload:' . CreateUserDataValidator::class);
+        $this->createUserDataValidator->shouldReceive('validate')->withArgs(
+            function (array $args): bool {
+                if (!isset($args['username']) ||
+                    $args['username'] !== 'UsernameValue'
+                ){
+                    return false;
+                }
+
+                return true;
+            }
+        )
+            ->once()
+            ->andReturn($returnValidate)
+        ;
+    }
+}
