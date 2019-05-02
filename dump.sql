681cd060866e39b44ae125d10ae7862c548020bc
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-02 21:58:07 +0200

    docker compose and dump.sql

--- dump.sql
+++ dump.sql
@@ -1,5 +1,5 @@
-CREATE TABLE "users" (
-    "id" INTEGER(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    "username" VARCHAR(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-    "created_at" TIMESTAMP COLLATE utf8mb4_unicode_ci  NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ;
+CREATE TABLE users(
+    user_id serial PRIMARY KEY,
+    username VARCHAR (64) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
