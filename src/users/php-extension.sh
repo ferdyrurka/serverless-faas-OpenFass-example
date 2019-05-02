288f4020386ab778d4ce8287a41d9917338b0fef
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-02 14:33:13 +0200

    mysql to psgql

--- src/users/php-extension.sh
+++ src/users/php-extension.sh
@@ -2,7 +2,8 @@
 
 echo "Installing PHP extensions"
 
-docker-php-ext-install json pdo_mysql
+apk --no-cache add postgresql-dev \
+&& docker-php-ext-install pdo pdo_pgsql pgsql json
 
 # list php extension
 # https://gist.github.com/giansalex/2776a4206666d940d014792ab4700d80
