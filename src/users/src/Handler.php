8dbc21606d76aca7a12fd0c468133649ac37b234
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-03 10:06:29 +0200

    Handler refactoring

--- src/users/src/Handler.php
+++ src/users/src/Handler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App;
 
 use App\Common\Database\ConnectionDatabase;
+use App\Common\Model\ResponseModel;
 use App\Exception\HttpException;
 use App\Exception\UndefinedTypeException;
 use App\Service\CreateUserService;
@@ -27,18 +28,7 @@ class Handler
                 throw new UndefinedTypeException();
             }
 
-            switch ($data['type']) {
-                case 'create':
-                    $userService = new CreateUserService();
-                    break;
-                case 'findAll':
-                    $userService = new FindAllUserService();
-                    break;
-                default:
-                    throw new UndefinedTypeException();
-            }
-
-            $responseModel = $userService->handle($data);
+            $responseModel = $this->handleService($data);
         } catch (HttpException $httpException) {
             return \json_encode([
                 'statusCode' => $httpException->getHttpCode(),
@@ -50,5 +40,26 @@ class Handler
             'statusCode' => $responseModel->getStatusCode(),
             'body' => $responseModel->getBody()
         ]);
+    }
+
+    /**
+     * @param array $data
+     * @return ResponseModel
+     * @throws UndefinedTypeException
+     */
+    private function handleService(array $data): ResponseModel
+    {
+        switch ($data['type']) {
+            case 'create':
+                $userService = new CreateUserService();
+                break;
+            case 'findAll':
+                $userService = new FindAllUserService();
+                break;
+            default:
+                throw new UndefinedTypeException();
+        }
+
+        return $userService->handle($data);
     }
 }
